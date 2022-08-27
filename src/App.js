d635dc7ad0fee67d1ee31daf6c789ba86b0218bc
Ikbal Alissa <M99alissa@gmail.com>
2022-08-27 11:30:35 +0100

    brands selector option integrated with search

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ function App() {
   const [products, setProducts] = useState([]);
   const [searchResult, setSearchResult] = React.useState(products);
   const [searchInput, setSearchInput] = React.useState("");
+  // const [sortBy, setSortBy] = React.useState("all");
+  const options = Array.from(new Set(products.map((p) => p.brand)));
 
   useEffect(() => {
     fetch("http://localhost:3001/api/products")
@@ -14,18 +16,39 @@ function App() {
   // console.log(products);
 
   function search(e) {
+    const brands = document.getElementById("brands");
+    // if statement for the brands selector
+    if (brands.value === "adidas") {
+      products.filter((product) => product.brand.includes("adidas"));
+    } else if (brands.value === "nike") {
+      products.filter((product) => product.brand.includes("nike"));
+    } else if (brands.value === "ASICS") {
+      console.log(brands.value);
+      products.filter((product) => product.brand.includes("ASICS"));
+    }
+    // if statement for the search values
     if (e.target.value.length >= 3) {
       setSearchInput(e.target.value);
       const results = products.filter(
         (product) =>
-          product.name.includes(e.target.value) ||
-          product.brand.includes(e.target.value) ||
-          product.description.includes(e.target.value)
+          product.name.includes(e.target.value && brands.value) ||
+          product.brand.includes(e.target.value && brands.value) ||
+          product.description.includes(e.target.value && brands.value)
       );
 
       setSearchResult(results);
-    } else {
+    } else if (e.target.value.length === 0) {
       setSearchResult(products);
+    }
+  }
+
+  function stock(e) {
+    if (e.target.value === "all") {
+      setSearchResult(products);
+    } else if (e.target.value === "in-stock") {
+      setSearchResult(products.filter((p) => p.quantity > 0));
+    } else if (e.target.value === "out-of-stock") {
+      setSearchResult(products.filter((p) => p.quantity === 0));
     }
   }
   return (
@@ -44,7 +67,11 @@ function App() {
           <div className="filter">
             <label htmlFor="brands">Brands</label>
 
-            <select id="brands"></select>
+            <select id="brands" onChange={search}>
+              {options.map((option) => (
+                <option>{option} </option>
+              ))}
+            </select>
           </div>
           <div className="filter">
             <label htmlFor="in-stock">All</label>
@@ -54,15 +81,24 @@ function App() {
               name="stock"
               value="all"
               defaultChecked={true}
+              onClick={stock}
             />
             <label htmlFor="in-stock">In stock</label>
-            <input type="radio" id="in-stock" name="stock" value="in-stock" />
+            <input
+              type="radio"
+              id="in-stock"
+              name="stock"
+              value="in-stock"
+              onClick={stock}
+            />
             <label htmlFor="out-of-stock">Out of stock</label>
+
             <input
               type="radio"
               id="out-of-stock"
               name="stock"
               value="out-of-stock"
+              onClick={stock}
             />
           </div>
         </div>
